a7ac2ae47f416f0a13267e5dbd47d35cb212bbb9
Tomáš Konrády <tomaskonrady@gmail.com>
2020-01-27 15:32:05 +0100

    SelectField

--- apps/storybook/src/components/Heading.stories.js
+++ apps/storybook/src/components/Heading.stories.js
@@ -1,10 +1,9 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
-import { action } from '@storybook/addon-actions';
 import { withKnobs } from '@storybook/addon-knobs/react';
-import { Heading } from '@lundegaard/ui-components';
+import { Heading } from '@fast-ai/ui-components';
 
 storiesOf('Heading', module)
 	.addDecorator(withKnobs)
-	.addParameters({ info: ''})
+	.addParameters({ info: '' })
 	.add('in default', () => <Heading />);
--- apps/storybook/src/components/SelectField.stories.js
+++ apps/storybook/src/components/SelectField.stories.js
@@ -1,10 +1,9 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
-import { action } from '@storybook/addon-actions';
 import { withKnobs } from '@storybook/addon-knobs/react';
-import { SelectField } from '@lundegaard/ui-components';
+import { SelectField } from '@fast-ai/ui-components';
 
 storiesOf('SelectField', module)
 	.addDecorator(withKnobs)
-	.addParameters({ info: ''})
+	.addParameters({ info: '' })
 	.add('in default', () => <SelectField />);
