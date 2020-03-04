c0b67555cd04fe2a63c0252d41baddf656e33aec
Tomáš Konrády <tomaskonrady@gmail.com>
2020-03-04 22:43:01 +0100

    chore: typo

--- tools/copy-files.js
+++ tools/copy-files.js
@@ -77,7 +77,7 @@ async function prepend(file, string) {
 }
 
 async function addLicense(packageData) {
-	const license = `/** @license Material-UI v${packageData.version}
+	const license = `/** @license Lundium v${packageData.version}
  *
  * This source code is licensed under the MIT license found in the
  * LICENSE file in the root directory of this source tree.
