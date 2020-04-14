adf003e0f8edccaa28d5e20a35aabcb7ddc67e04
Tomáš Konrády <tomaskonrady@gmail.com>
2020-04-14 12:30:56 +0200

    chore: es-build

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,21 @@
+let defaultPresets;
+
+// We release a ES version of Material-UI.
+// It's something that matches the latest official supported features of JavaScript.
+// Nothing more (stage-1, etc), nothing less (require, etc).
+if (process.env.BABEL_ENV === 'es') {
+	defaultPresets = [];
+} else {
+	defaultPresets = [
+		[
+			'@babel/preset-env',
+			{
+				bugfixes: true,
+				modules: ['esm', 'production-umd'].includes(process.env.BABEL_ENV) ? false : 'commonjs',
+			},
+		],
+	];
+}
 const productionPlugins = [
 	[
 		'@babel/plugin-transform-react-constant-elements',
@@ -11,25 +29,47 @@ const productionPlugins = [
 	],
 ];
 
-const plugins = [...productionPlugins, ['@babel/plugin-transform-runtime', { useESModules: true }]];
-
-const config = { plugins };
-
 module.exports = {
-	presets: [
-		[
-			'babel-preset-react-union',
-			{ test: process.env.NODE_ENV === 'test', loose: true, library: false, universal: false },
-		],
+	presets: defaultPresets.concat(['@babel/preset-react']),
+	plugins: [
+		['@babel/plugin-proposal-class-properties', { loose: true }],
+		['@babel/plugin-proposal-object-rest-spread'],
+		// any package needs to declare 7.4.4 as a runtime dependency. default is ^7.0.0
+		['@babel/plugin-transform-runtime', { version: '^7.4.4' }],
+		// for IE 11 support
+		'@babel/plugin-transform-object-assign',
 	],
-	ignore: [/@babel[\\|/]runtime/], // Fix a Windows issue.
 	env: {
 		cjs: {
 			plugins: productionPlugins,
 		},
-		esm: config,
-		es: config,
-		production: config,
-		'production-umd': config,
+		development: {
+			plugins: [
+				[
+					'babel-plugin-module-resolver',
+					{
+						alias: {
+							modules: './modules',
+						},
+					},
+				],
+			],
+		},
+		esm: {
+			plugins: [...productionPlugins, ['@babel/plugin-transform-runtime', { useESModules: true }]],
+		},
+		es: {
+			plugins: [...productionPlugins, ['@babel/plugin-transform-runtime', { useESModules: true }]],
+		},
+		production: {
+			plugins: [...productionPlugins, ['@babel/plugin-transform-runtime', { useESModules: true }]],
+		},
+		'production-umd': {
+			plugins: [...productionPlugins, ['@babel/plugin-transform-runtime', { useESModules: true }]],
+		},
+		test: {
+			sourceMaps: 'both',
+		},
 	},
+	ignore: [/@babel[\\|/]runtime/], // Fix a Windows issue.
 };
