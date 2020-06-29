33701656578dc321000ba05c836f9c6c04c3c37a
Tomáš Konrády <tomaskonrady@gmail.com>
2020-06-29 12:39:30 +0200

    fix: Grid - Row - reading gutters from grid

--- packages/ui-components/src/Row/Row.js
+++ packages/ui-components/src/Row/Row.js
@@ -1,7 +1,13 @@
 import React from 'react';
 
+import mapResponsiveProperty from '../utils/mapResponsiveProperty';
 import Flex from '../Flex';
+import useTheme from '../hooks/useTheme';
 
-const Row = props => <Flex mx={-2} {...props} />;
+const Row = (props) => {
+	const { grid: { gutters = 2 } = {} } = useTheme();
+
+	return <Flex mx={mapResponsiveProperty((x) => -x, gutters)} {...props} />;
+};
 
 export default Row;
