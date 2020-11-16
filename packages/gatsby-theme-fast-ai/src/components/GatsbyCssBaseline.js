ced2ca9aa629236df6d462b84be2b6460ec73bb3
Tomáš Konrády <tomaskonrady@gmail.com>
2020-11-16 08:46:36 +0100

    feat: GatsbyCssBaseline fixes problems with 100% height of the main container

--- packages/gatsby-theme-fast-ai/src/components/GatsbyCssBaseline.js
+++ packages/gatsby-theme-fast-ai/src/components/GatsbyCssBaseline.js
@@ -1,14 +1,22 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { CssBaseline } from '@fast-ai/ui-components';
 
-const GatsbyCssBaseline = () => (
+const GatsbyCssBaseline = ({ styles = '', ...rest }) => (
 	<CssBaseline
 		styles={`
-			#___gatsby, div[role="group"][tabindex] {
+			#___gatsby, #gatsby-focus-wrapper, div[role="group"][tabindex] {
 			    min-height: 100%;
+					height: 100%;
 			}
+			${styles}
 			`}
+		{...rest}
 	/>
 );
+
+GatsbyCssBaseline.propTypes = {
+	styles: PropTypes.string,
+};
 
 export default GatsbyCssBaseline;
